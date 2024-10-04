53bff68c11555ccf4c22fd7ee60c6678b63e5546
Win-ther <emil.winther@hotmail.com>
2024-10-04 12:27:11 +0200

    Add header, nav, main and footer and styling with tailwind.
    Co-authored-by: Katherine Altmann <katherine.altmann@iths.se>
    Co-authored-by: Alfred Bild <alfredbild@gmail.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import logo from '@/assets/logo.png'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <header class="bg-old-green p-2 shadow-md flex items-center">
+    <img :src="logo" alt="Careta logo icon: Vintage old car" class="size-20"/>
+    <h1 class="font-bold text-4xl">CARETA</h1>
+  </header>
+  <nav class="bg-old-green grid grid-rows-3 gap-0 items-center font-bold">
+    <RouterLink class="border-black border-t  text-center p-2" to="/">Home</RouterLink>
+    <RouterLink class="border-black border-y text-center p-2" to="/">Rent Cars</RouterLink>
+    <RouterLink class="border-black border-b text-center p-2" to="/">Cancel Car</RouterLink>
+  </nav>
+  <main class="bg-amber-50 p-6 flex flex-col gap-5 ">
+    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+</main>
+  <footer class="bg-old-black p-5 text-center" >
+    <p>© Grupp 6</p>
+  </footer>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,tsx,vue,jsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Helvetica','Arial','sans-serif'],
-      },
+    content: ['./index.html', './src/**/*.{js,ts,tsx,vue,jsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Helvetica', 'Arial', 'sans-serif'],
+            },
+            colors: {
+                'sooty-yellow': '#ece5cf',
+                'old-red': '#ffdac6',
+                'old-yellow': '#ffe7c2',
+                'old-green': '#babd8d',
+                'old-blue': '#d8e2dc',
+                'old-black': '#aeae98'
+            },
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
 
