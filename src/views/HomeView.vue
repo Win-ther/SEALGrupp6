5b58823ba8b169bc5e82e054b363b20efb9fb88d
Win-ther <emil.winther@hotmail.com>
2024-10-07 13:55:29 +0200

    Add styling and logo, mediaqueries, change metainfo
    Co-authored-by: Katherine Altmann <katherine.altmann@iths.se>
    Co-authored-by: Alfred Bild <alfredbild@gmail.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,58 @@
 <script setup>
-import logo from '@/assets/logo.png'
+import logo from '@/assets/CaretaLogo.png'
+import {useRoute} from "vue-router";
+
+const isActiveLink = (routePath) => {
+  const route = useRoute();
+  return route.path === routePath;
+}
 </script>
 
 <template>
-  <header class="bg-old-green p-2 shadow-md flex items-center gap-3">
-    <RouterLink to="/">
-    <img :src="logo" alt="Careta logo icon: Vintage old car" class="w-[120px]"/>
-    </RouterLink>
-    <h1><span class="font-logo text-4xl">Careta - </span><span class="text-2xl">Rent it like you own it.. return it because you don't!</span> </h1>
+  <header
+      class="bg-old-green grid grid-rows-1 desktop:flex desktop:gap-3 desktop:justify-start desktop:items-end desktop:pb-1 desktop:pt-3 desktop:pl-4">
+
+      <img :src="logo" alt="Careta logo icon: Vintage old car" class="w-40 m-auto mt-2 mb-2 desktop:m-0"/>
+
+    <nav class="bg-old-green grid grid-rows-3 items-stretch desktop:flex desktop:gap-1 desktop:justify-center">
+      <RouterLink
+          :class="[isActiveLink('/')?'underline underline-offset-8 bg-button-green':'', 'p-2', 'border-old-black', 'border-t', 'desktop:border-none', 'hover:bg-button-green', 'text-center' ]"
+          to="/">Home
+      </RouterLink>
+      <RouterLink
+          :class="[isActiveLink('/rentcars')?'underline underline-offset-8 bg-button-green':'', 'p-2', 'border-old-black', 'border-y', 'desktop:border-none', 'hover:bg-button-green', 'text-center']"
+          to="/">Rent a Careta
+      </RouterLink>
+      <RouterLink
+          :class="[isActiveLink('/cancelcar')?'underline underline-offset-8 bg-button-green':'', 'p-2 ', 'border-old-black', 'border-b', 'desktop:border-none', 'hover:bg-button-green', 'text-center']"
+          to="/">Cancel Reservation
+      </RouterLink>
+    </nav>
   </header>
-  <nav class="bg-old-green grid grid-rows-3 gap-0 font-bold items-stretch">
-    <RouterLink class="border-black p-2 border-t text-center " to="/">Home</RouterLink>
-    <RouterLink class="border-black p-2 border-y text-center" to="/">Rent Cars</RouterLink>
-    <RouterLink class="border-black p-2 border-b text-center " to="/">Cancel Car</RouterLink>
-  </nav>
   <main class="bg-amber-50 p-6 flex flex-col gap-5 h-auto">
-    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+      laborum."</p>
+    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+      laborum."</p>
+    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+      laborum."</p>
   </main>
-  <footer class="bg-old-black p-5 text-center " >
-    <p>© Grupp 6</p><br>
+  <footer class="bg-old-black p-5 text-center ">
+    <p>Careta © Grupp 6</p><br>
     <div class="flex gap-5 justify-center underline text-xs">
-    <RouterLink class="border-black text-center " to="/">Home</RouterLink>
-    <RouterLink class="border-black text-center" to="/">Rent Cars</RouterLink>
-    <RouterLink class="border-black text-center text-" to="/">Cancel Car</RouterLink>
-    <a class="border-black text-center text-" href="https://www.youtube.com/watch?v=Eo-g3JkikFc">Full Careta</a>
+      <RouterLink class="border-black text-center " to="/">Home</RouterLink>
+      <RouterLink class="border-black text-center" to="/">Rent a Careta</RouterLink>
+      <RouterLink class="border-black text-center text-" to="/">Cancel Reservation</RouterLink>
+      <a class="border-black text-center text-" href="https://www.youtube.com/watch?v=Eo-g3JkikFc">Full Careta</a>
     </div>
   </footer>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,14 @@ export default {
                 'old-yellow': '#ffe7c2',
                 'old-green': '#babd8d',
                 'old-blue': '#d8e2dc',
-                'old-black': '#aeae98'
+                'old-black': '#aeae98',
+                'button-green': '#c1c491'
+                //'button-green': '#d0d49f'
+                //'button-green': '#c6c99b'
             },
+        },
+        screens: {
+            'desktop': '700px',
         },
     },
     variants: {
