52feb27fc989b04b24249eae41ffa3c8d44869dd
Win-ther <emil.winther@hotmail.com>
2024-10-08 11:17:27 +0200

    Change footer so it is not fixed, add RentACareta.vue with temp text, add it to routing.
    Co-authored-by: Katherine Altmann <katherine.altmann@iths.se>
    Co-authored-by: Alfred Bild <alfredbild@gmail.com>

---
+++ src/views/RentACareta.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class="grid ">
+    <p class="text-red-700 text-2xl">TEMPORÄR TEXT, DETTA SKA ERSÄTTAS MED FRÄSIGA BILAR</p>
+  </main>
+</template>
+
+<style>
+</style>
